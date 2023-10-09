43b8c2eb516b6d63ba93312618bb7560d7bf0048
salman yuris <salmanyuris45@gmail.com>
2023-10-09 12:35:17 +0700

    membenahi rumus 1.0

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function result() {
     let input = document.getElementById("inputNumber").value;
     let output = document.getElementById('result');
     let x = Number(input);
-    let y = x * 0.24 + 0.42;
+    // let y = x * 0.24 + 0.42;
+    let y = x * 0.23 + 2.37;
     output.innerHTML = Math.round(y);
     console.info(input);
 }
